217613a8d6b13c0b47527c0f7e82783f827eb9a0
Jatin Behl <jatinbehl@gmail.com>
2014-01-18 16:58:34 -0500

    sdcs

--- NewBasicGesture/src/com/example/basicgesture/MainActivity.java
+++ NewBasicGesture/src/com/example/basicgesture/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends Activity implements SensorEventListener {
 	int counter = 0;
 	int foundSame = 0;
 	@Override
+	//dsfs
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
