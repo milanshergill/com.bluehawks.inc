30420e2640cf224f8861b738d52d5b76b7d1739e
jatinbehl <jatinbehl@ip-10-24-2-051.airyork.yorku.ca>
2014-01-23 15:15:34 -0500

    1ST commit

--- ENG4KGestures/src/com/example/eng4kgestures/MainActivity.java
+++ ENG4KGestures/src/com/example/eng4kgestures/MainActivity.java
@@ -1,23 +1,16 @@
-package com.example.scratchgestures;
+package com.example.eng4kgestures;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
 
 public class MainActivity extends Activity {
-	
-
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
 	}
-	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
