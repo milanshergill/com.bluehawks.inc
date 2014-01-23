efc52cb2ee555441f68b59850353b009f638f6bd
Milan <milandeepshergill@gmail.com>
2014-01-23 16:21:52 -0500

    testing layout - minor changes

--- ENG4KGestures/src/com/example/eng4kgestures/TestGestures.java
+++ ENG4KGestures/src/com/example/eng4kgestures/TestGestures.java
@@ -1,8 +1,11 @@
 package com.example.eng4kgestures;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -13,25 +16,38 @@ public class TestGestures extends Activity {
 	TextView results;
 	ListView resultsList;
 	
+	//LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+    ArrayList<String> listItems = new ArrayList<String>();
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.test_gestures);
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
 		
 		start = (Button) findViewById(R.id.start);
 		start = (Button) findViewById(R.id.stop);
 		results = (TextView) findViewById(R.id.resultsView);
 		resultsList = (ListView) findViewById(R.id.resultsList);
+		
+		resultsList.setAdapter(adapter);
 	}
 	
 	public void onClickStart(View v) {
-		//Start Testing
-		results.setVisibility(0);
+		// Start Testing
+		addItem("Milan");
 	}
 	
 	public void onClickStop(View v) {
 		// Stop Testing and add results to the resultsList
-		results.setVisibility(1);
+	}
+	
+	public void addItem(String value) {
+		listItems.add(value);
+		adapter.notifyDataSetChanged();
 	}
 }
