fed33aebe867294b1abf0aa70061596e191f26e0
jatinbehl <jatinbehl@ip-10-24-2-051.airyork.yorku.ca>
2014-01-23 16:02:35 -0500

    commit 2

--- ENG4KGestures/src/com/example/eng4kgestures/MainActivity.java
+++ ENG4KGestures/src/com/example/eng4kgestures/MainActivity.java
@@ -2,14 +2,22 @@ package com.example.eng4kgestures;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	Button RecordGestures;
+	Button TestGestures;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		RecordGestures =  (Button) (findViewById((R.id.RecordGestures))) ;
+		TestGestures =  (Button) (findViewById((R.id.TestGestures))) ;
 	}
 
 	@Override
@@ -19,4 +27,8 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public void onClickRecordGestures(View v) {
+	Intent myIntent = new Intent(MainActivity.this, RecordGestures.class);
+	MainActivity.this.startActivity(myIntent);
+	}
 }
