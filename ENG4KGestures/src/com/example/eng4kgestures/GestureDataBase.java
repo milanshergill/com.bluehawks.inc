c0fe0a694d868ef534bcbc71c90619a044142b61
Milan <milandeepshergill@gmail.com>
2014-01-25 17:42:13 -0500

    Updated Database

---
+++ ENG4KGestures/src/com/example/eng4kgestures/GestureDataBase.java
@@ -0,0 +1,84 @@
+package com.example.eng4kgestures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+public class GestureDataBase {
+
+	// Database fields
+	private SQLiteDatabase database;
+	private MySQLiteHelper dbHelper;
+	private String[] allColumns = { MySQLiteHelper.COLUMN_ID,
+			MySQLiteHelper.COLUMN1_COMMENT };
+
+	public GestureDataBase(Context context) {
+		dbHelper = new MySQLiteHelper(context);
+	}
+
+	public void open() throws SQLException {
+		database = dbHelper.getWritableDatabase();
+	}
+
+	public void close() {
+		dbHelper.close();
+	}
+
+	public long insertGesture(Gesture gesture) {
+		ContentValues values = new ContentValues();
+		byte[] blob = Gesture.serializeGesture(gesture);
+		values.put(MySQLiteHelper.COLUMN1_COMMENT, blob);
+		
+		return database.insert(MySQLiteHelper.GESTURE_TABLE, null,
+				values);
+	}
+	
+	public Gesture retriveGesture() throws SQLException {
+		Cursor cur = database.query(true, MySQLiteHelper.GESTURE_TABLE, allColumns, null, null, null, null, null, null);
+		if (cur.moveToFirst()) {
+			byte[] blob = cur.getBlob(cur.getColumnIndex(MySQLiteHelper.COLUMN1_COMMENT));
+			cur.close();
+			return Gesture.deserializeGesture(blob);
+		}
+		cur.close();
+		return null;
+	}
+
+	public void deleteGesture(long id) {
+		System.out.println("Gesture deleted with id: " + id);
+		database.delete(MySQLiteHelper.GESTURE_TABLE, MySQLiteHelper.COLUMN_ID
+				+ " = " + id, null);
+	}
+
+//	public List<Acceleration> getAllAccelerations() {
+//		List<Acceleration> accelerations = new ArrayList<Acceleration>();
+//
+//		Cursor cursor = database.query(MySQLiteHelper.TABLE_COMMENTS,
+//				allColumns, null, null, null, null, null);
+//
+//		cursor.moveToFirst();
+//		while (!cursor.isAfterLast()) {
+//			Acceleration acceleration = cursorToAcceleration(cursor);
+//			accelerations.add(acceleration);
+//			cursor.moveToNext();
+//		}
+//		// make sure to close the cursor
+//		cursor.close();
+//		return accelerations;
+//	}
+
+//	private Acceleration cursorToAcceleration(Cursor cursor) {
+//		Acceleration acceleration = new Acceleration();
+//		acceleration.setId(cursor.getLong(0));
+//		acceleration.setName(cursor.getString(1));
+//		acceleration.setAccelerationX(cursor.getFloat(2));
+//		acceleration.setAccelerationY(cursor.getFloat(3));
+//		acceleration.setAccelerationZ(cursor.getFloat(4));
+//		return acceleration;
+//	}
+}
--- ENG4KGestures/src/com/example/eng4kgestures/MySQLiteHelper.java
+++ ENG4KGestures/src/com/example/eng4kgestures/MySQLiteHelper.java
@@ -7,24 +7,18 @@ import android.util.Log;
 
 public class MySQLiteHelper extends SQLiteOpenHelper {
 
-  public static final String TABLE_COMMENTS = "acceleration";
+  public static final String GESTURE_TABLE = "GesturesTable";
   public static final String COLUMN_ID = "_id";
-  public static final String COLUMN2_COMMENT = "Ax";
-  public static final String COLUMN3_COMMENT = "Ay";
-  public static final String COLUMN4_COMMENT = "Az";
-  public static final String COLUMN1_COMMENT = "name";
+  public static final String COLUMN1_COMMENT = "GestureObjects";
 
-  private static final String DATABASE_NAME = "acceleration.db";
+  private static final String DATABASE_NAME = "gestures.db";
   private static final int DATABASE_VERSION = 1;
 
   // Database creation sql statement
   private static final String DATABASE_CREATE = "create table "
-      + TABLE_COMMENTS + "(" + COLUMN_ID
+      + GESTURE_TABLE + "(" + COLUMN_ID
       + " integer primary key autoincrement, " + COLUMN1_COMMENT
-      + " text not null ," + COLUMN2_COMMENT
-      + " FLOAT not null," + COLUMN3_COMMENT
-      + " FLOAT not null," + COLUMN4_COMMENT
-      + " FLOAT not null  );";
+      + " BLOB not null );";
 
   public MySQLiteHelper(Context context) {
     super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -40,7 +34,7 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
     Log.w(MySQLiteHelper.class.getName(),
         "Upgrading database from version " + oldVersion + " to "
             + newVersion + ", which will destroy all old data");
-    db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS);
+    db.execSQL("DROP TABLE IF EXISTS " + GESTURE_TABLE);
     onCreate(db);
   }
 
