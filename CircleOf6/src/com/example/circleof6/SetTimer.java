0648edd6ffe243dba3dd589a0fa3584b10440ede
jatinbehl <jatinbehl@ip-10-24-6-072.airyork.yorku.ca>
2014-03-12 15:44:52 -0400

    timer basic implementation

--- CircleOf6/src/com/example/circleof6/SetTimer.java
+++ CircleOf6/src/com/example/circleof6/SetTimer.java
@@ -1,15 +1,35 @@
 package com.example.circleof6;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.util.Calendar;
 
+
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Dialog;
+//import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+@SuppressLint("NewApi")
 public class SetTimer extends Activity {
+	private static TextView tvDisplayTime;
+	private Button btnChangeTime;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_set_timer);
+		tvDisplayTime = (TextView) findViewById(R.id.tvDisplayTime);
+		setCurrentTimeOnView(7,7);
+
 	}
 
 	@Override
@@ -17,6 +37,50 @@ public class SetTimer extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.set_timer, menu);
 		return true;
+		
+	}
+	
+	@SuppressLint("NewApi")
+	public void showTimePickerDialog(View v) {
+	    DialogFragment newFragment = new TimePickerFragment();
+	    newFragment.show(getFragmentManager(), "timePicker");
 	}
 
+	// display current time
+	public static void setCurrentTimeOnView(int hour,int minute) {
+		// set current time into textview
+		tvDisplayTime.setText(new StringBuilder().append(padding_str(hour))
+				.append(":").append(padding_str(minute)));
+
+
+	}
+
+	private static String padding_str(int c) {
+		if (c >= 10)
+			return String.valueOf(c);
+		else
+			return "0" + String.valueOf(c);
+	}
+
+}
+
+class TimePickerFragment extends DialogFragment implements
+		TimePickerDialog.OnTimeSetListener {
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		// Use the current time as the default values for the picker
+		final Calendar c = Calendar.getInstance();
+		int hour = c.get(Calendar.HOUR_OF_DAY);
+		int minute = c.get(Calendar.MINUTE);
+
+		// Create a new instance of TimePickerDialog and return it
+		return new TimePickerDialog(getActivity(), this, hour, minute,
+				DateFormat.is24HourFormat(getActivity()));
+	}
+
+	public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+		// Do something with the time chosen by the user
+		SetTimer.setCurrentTimeOnView(hourOfDay,minute);
+	}
 }
