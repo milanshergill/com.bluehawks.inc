d795fe59875bafe0cafbb14b9499965f5087d2b4
Milan <milandeepshergill@gmail.com>
2014-04-08 13:03:12 -0400

    minor changes

--- SafetyProject/src/com/example/safetyproject/SendDataToServer.java
+++ SafetyProject/src/com/example/safetyproject/SendDataToServer.java
@@ -19,7 +19,7 @@ public class SendDataToServer extends Activity {
 	private ProgressDialog pDialog;
 	// URL to which to send JSON data
 	// private static String url = "http://10.24.2.26:8080/GoogleMap/helpMe";
-	private static String url = "http://107.170.96.216:8888";
+	private static String url = "http://107.170.96.216:8888/start";
 	private static final String TAG_NAME = "name";
 	private static final String TAG_AGE = "age";
 	private static final String TAG_HEALTH = "health";
